ba808ddd87a5589ea64aadc58b6e1ee298134c05
TiamatCod3 <ti_matos@id.uff.br>
2022-10-12 20:03:21 -0300

    REFS por Tiago Matos: Criação do banco de dados e configuração

--- src/main/java/com/tiago/cursomc/domain/Categoria.java
+++ src/main/java/com/tiago/cursomc/domain/Categoria.java
@@ -3,14 +3,21 @@ package com.tiago.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
 	public Categoria() {
-		
+
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -51,6 +58,5 @@ public class Categoria implements Serializable{
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
